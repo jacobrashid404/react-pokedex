dc35901382ef8b02d7633a6cd51cf906a3809fae
Jacob <jacobrashid404@gmail.com>
2024-05-27 17:31:47 -0700

    added custom alt image

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -2,16 +2,18 @@ import './Pokecard.css';
 
 /** Generates card for a Pokemon
  * Props: 'Pokemon' object => {name, id, type, base_experience}
- * Returns: Pokemon's name, image, type and base experience in a div
+ * Renders: Pokegame => Pokedex => Pokecard
  */
 function Pokecard({ pokemon }) {
     const pokeImg =
         `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
 
+    const altImgText = `${pokemon.name} image`;
+
     return (
         <div className='Pokecard'>
             <h1>{ pokemon.name }</h1>
-            <img src={pokeImg} alt="pokemon image"></img>
+            <img src={pokeImg} alt={altImgText}></img>
             <p>Type: { pokemon.type }</p>
             <p>EXP: { pokemon.base_experience }</p>
         </div>
