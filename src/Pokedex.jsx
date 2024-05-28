fee5756c231531f09837fdfc355b20734c981524
Jacob <jacobrashid404@gmail.com>
2024-05-27 17:31:57 -0700

    updated docstring

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -3,15 +3,15 @@ import Pokecard from "./Pokecard.jsx";
 /** Pokedex component
  *
  * Props: array of 'Pokemon' objects => [{name, id, type, base_experience}, ...]
- * Returns: div containing sub-divs with Pokemon info
+ *        isWinner boolean
+ * Renders: Pokegame => Pokedex => Pokecard
  *
  */
 function Pokedex({ pokemon, isWinner }) {
     return (
         <div className="row row-cols-5">
-            { pokemon.map(p => <Pokecard pokemon={p}/>)}
-            <p>{isWinner ? "This hand wins!" : ""}</p>
-            <p></p>
+            { pokemon.map(p => <Pokecard pokemon={p}/>) }
+            <b>{isWinner ? "This hand wins!" : ""}</b>
         </div>
     );
 }
